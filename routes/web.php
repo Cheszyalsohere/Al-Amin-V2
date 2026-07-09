11e1a3ae80dd7cc410b418e2169590f0fc340570
Cheszy <muhammadirfannuha18@gmail.com>
2026-07-09 12:56:12 +0700

    chore: remove dead account-deletion capability (single-admin)

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ Route::redirect('/dashboard', '/admin')->middleware(['auth', 'verified'])->name(
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware('auth')->prefix('admin')->group(function () {
